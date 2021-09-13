725b25eae317f1ec99bd5923da933a49a5938497
Brandon Prange <bprange93@gmail.com>
2021-09-13 15:03:08 -0500

    Working with API to be called. Current issue is that key is not callable as a string. Looking into tutorials to see how to call in key to be used.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
+from YelpAPI import api_key
+
+API_KEY = api_key()
+ENDPOINT = 'https://api.yelp.com/v3/businesses/{id}/reviews'
+HEADERS = {'Authorization': 'bearer %s' % api_key}
+
+PARAMETERS = {'id': 'l7mNzyV3pOZ3m87jRY6J7A'}
 
 
-def run_api():
-    response = requests.get(
-        "https://api.yelp.com/v3/businesses/l7mNzyV3pOZ3m87jRY6J7A/reviews")
-    print(response.json())
+response = requests.get(url=ENDPOINT, params=PARAMETERS, headers=HEADERS)
+print(response.json())
