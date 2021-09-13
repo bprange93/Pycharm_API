47477c4cadc79e6482954b8eee821b6db89bb8be
Brandon Prange <bprange93@gmail.com>
2021-09-13 15:22:08 -0500

    Pulled API key into main.py because it wasn't importing from the YelpAPI class correctly. Kept being pulled in as a string and wouldn't use as a key. Now trying to return the response as a list instead of showing it all on one line.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 from YelpAPI import api_key
 
-API_KEY = api_key()
-ENDPOINT = 'https://api.yelp.com/v3/businesses/{id}/reviews'
+API_KEY = "X9N5k658alNxj9wvD0l5IFtG7If-WIUDA7R_x06BVGzAc0pQ7IvvIV15Hurp6gIfzubjHD-6W69Yf8oEVj8pH5V49t1ctBwvAHox4cPw2f" \
+          "5N738Ku1CLlBW4Moo7YXYx"
+ENDPOINT = 'https://api.yelp.com/v3/businesses/l7mNzyV3pOZ3m87jRY6J7A/reviews'
 HEADERS = {'Authorization': 'bearer %s' % api_key}
 
-PARAMETERS = {'id': 'l7mNzyV3pOZ3m87jRY6J7A'}
 
-
-response = requests.get(url=ENDPOINT, params=PARAMETERS, headers=HEADERS)
-print(response.json())
+response = requests.get(url=ENDPOINT, headers=HEADERS)
+reviews = response.json()
+print(reviews['list'])
